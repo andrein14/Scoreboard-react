05525dc916c883a87882baa7a30f67a9b7eb5a64
alin_g1000 <alin_g1000@yahoo.com>
2019-01-22 19:21:13 +0200

    Updates

---
+++ precache-manifest.19dc4b2ce18f664b717d813f190484a9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0db2712ec534f4b030d",
+    "url": "/React-projects/2.3/static/js/runtime~main.a0db2712.js"
+  },
+  {
+    "revision": "9eb435ecc8eb7fb23558",
+    "url": "/React-projects/2.3/static/js/main.9eb435ec.chunk.js"
+  },
+  {
+    "revision": "fe65e5159218a8a08e5e",
+    "url": "/React-projects/2.3/static/js/1.fe65e515.chunk.js"
+  },
+  {
+    "revision": "609e8fb1b6a01b63f517b9a562a0da1d",
+    "url": "/React-projects/2.3/index.html"
+  }
+];
